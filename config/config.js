c49f8bba28f4bb4084e7a77f1c9ea6674ccb539d
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2018-05-19 21:57:36 +0800

    Organize project structure

---
+++ config/config.js
@@ -0,0 +1,56 @@
+'use strict';
+
+exports.serverConfig = {
+    port: 6477,
+    host: '127.0.0.1'
+};
+
+exports.jwtConfig = {
+    secret: 'mts-webservice',
+    algorithm: 'HS256',
+    expiresIn: 60*60*24*7
+};
+
+exports.socketConfig = {
+    port: 6477,
+    host: '127.0.0.1'
+};
+
+/* Local Redis */
+exports.redisConfig = {
+    port: 6379,
+    host: 'localhost',
+    password: 'pwd@redis143'
+};
+
+/* Remote Redis */
+// exports.redisConfig = {
+//     port: ,
+//     host: '',
+//     password: ''
+// };
+
+/* Local Database */
+
+exports.dbConfig = {
+    connectionLimit: 100,
+    host: 'localhost',
+    user: 'solarsuite',
+    password: 'pwd@solarsuite',
+    database: 'solarsuite',
+    debug: false
+};
+
+/* Heroku Database */
+// exports.dbConfig = {
+//     connectionLimit: 100,
+//     host: '',
+//     user: '',
+//     password: '',
+//     database: '',
+//     debug: false
+// };
+
+exports.bcryptConfig = {
+    rounds: 10
+};
